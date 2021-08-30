78a18b5e1c697865d72dfa788b0906e50b31beb1
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-30 08:05:25 -0500

    Se agrega los servicios de autenticacion al front

--- frontend/src/app/home/login/login.component.ts
+++ frontend/src/app/home/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData: any;
+  message: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number=2;
+
+  constructor(private _userService: UserService,
+    private _router: Router,
+    private _snackBar: MatSnackBar) {
+      this.message = '';
+      this.loginData = {};
+     }
 
   ngOnInit(): void {
   }
 
+  login(){
+    if (!this.loginData.email || !this.loginData.password) {
+      console.log('Failed process: Incomplete data');
+      this.message = 'Failed process: Incomplete data';
+      this.openSnackBarError();
+      this.loginData = {};
+   } else {
+     this._userService.login(this.loginData).subscribe(
+       (res) =>{
+         console.log(res);
+         localStorage.setItem('token', res.jwtToken)
+         this._router.navigate(['/listTask']);
+         this.loginData={};
+       },
+       (err)=>{
+         console.log(err);
+         this.message = err.error
+         this.openSnackBarError();
+       }
+     )
+   }
+  };
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse']
+    })
+  }
 }
